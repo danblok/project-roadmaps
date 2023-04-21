066e1b6480afda641f6339d8ae669145be8d76eb
vacatixn <danytokmakov@gmail.com>
2023-04-21 15:37:17 +0500

    feat: add auth options in auth routes

---
+++ apps/web/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,28 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+
+  ],
+  theme: {
+    colorScheme: 'light'
+  },
+  callbacks: {
+    async jwt({ token }) {
+      return token
+    },
+  }
+}
+
+export default NextAuth(authOptions);
