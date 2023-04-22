bbe57f5daf27b11c09125b468a84e106352145c3
vacatixn <danytokmakov@gmail.com>
2023-04-22 23:15:02 +0500

    feat: add session contex

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,9 +1,15 @@
 import '@/styles/global.css'
+import { Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
 import { type AppProps } from 'next/app'
 
 export default function App({
   Component,
-  pageProps,
-}: AppProps) {
-  return <Component {...pageProps} />
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
