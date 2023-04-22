e20b849bc414df028dcfc39c9909451c055a11e2
vacatixn <danytokmakov@gmail.com>
2023-04-22 23:54:06 +0500

    feat: add layout on the index page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,15 +1,18 @@
+import Layout from '@/components/layout'
 import clsx from 'clsx'
 
 export default function Home() {
   return (
-    <div
-      className={clsx(
-        'text-2xl font-bold text-red-500',
-        'flex justify-center items-center',
-        'h-screen w-screen'
-      )}
-    >
-      Home
-    </div>
+    <Layout>
+      <div
+        className={clsx(
+          'text-2xl font-bold text-red-500',
+          'flex justify-center items-center',
+          'h-screen w-screen'
+        )}
+      >
+        Home
+      </div>
+    </Layout>
   )
 }
