986cb669ea9e3f50c34b9837ba98351f0ae708c3
vacatixn <danytokmakov@gmail.com>
2023-04-26 17:17:06 +0500

    feat: config tailwind default styles

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -8,7 +8,20 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cinereous: '#7E6C6C',
+        'light-red': '#F87575',
+        melon: '#FFA9A3',
+        'uranian-blue': '#B9E6FF',
+        'cornflower-blue': '#5C95FF',
+        'tifanny-blue': '#99E1D9',
+        'bittersweet': '#F25F5C'
+      },
+      gridTemplateRows: {
+        'main-layout': 'auto 1fr auto'
+      }
+    },
   },
   plugins: [
     formsPlugin
