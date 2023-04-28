9bdbd409dcc0b41d2c6c5d3874f05430abdbc8ec
vacatixn <danytokmakov@gmail.com>
2023-04-28 14:28:32 +0500

    feat: add query client

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,14 +2,25 @@ import '@/styles/global.css'
 import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import { type AppProps } from 'next/app'
+import Layout from './layout'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
