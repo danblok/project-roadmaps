d3bb6e15140f972f416876010263b554d74e0710
vacatixn <danytokmakov@gmail.com>
2023-05-04 13:10:21 +0500

    feat: update project api handler

--- apps/web/pages/api/project/[id].ts
+++ apps/web/pages/api/project/[id].ts
@@ -2,20 +2,104 @@ import { prisma } from "database";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (!req.query.id || typeof req.query.id !== 'string') {
+    return res.status(404).json({ error: 'query error: missing id' })
+  }
+
   if (req.method === 'GET') {
-    const project = prisma.project.findUnique({
-      where: req.query
+    const project = await prisma.project.findUnique({
+      where: {
+        id: req.query.id,
+      },
+      include: {
+        contributors: true,
+        owner: true,
+        statuses: true,
+        tasks: true,
+      },
     })
 
     return res.status(200).send({ data: project })
   }
 
-  if (req.method === 'DELETE') {
-    const project = prisma.project.delete({
-      where: req.query
+  if (
+    req.method === 'PATCH'
+    && req.query.contributorId
+    && typeof req.query.contributorId === 'string'
+  ) {
+    await prisma.project.update({
+      where: {
+        id: req.query.id,
+      },
+      data: {
+        contributors: {
+          connect: {
+            id: req.query.contributorId
+          }
+        }
+      }
     })
+    return res.status(200).send({})
+  }
 
-    return res.status(200).send({ data: project })
+
+  if (req.method === 'PATCH') {
+    await prisma.project.update({
+      where: {
+        id: req.query.id,
+      },
+      data: {
+        ...req.body
+      }
+    })
+    return res.status(200).send({})
+  }
+
+  if (req.method === 'DELETE'
+    && req.query.contributorId
+    && typeof req.query.contributorId === 'string'
+  ) {
+    await prisma.project.update({
+      where: {
+        id: req.query.id,
+      },
+      data: {
+        contributors: {
+          disconnect: {
+            id: req.query.contributorId
+          }
+        }
+      }
+    })
+    return res.status(200).send({})
+  }
+
+  if (req.method === 'DELETE') {
+    await prisma.$transaction([
+      prisma.project.update({
+        where: {
+          id: req.query.id
+        },
+        data: {
+          contributors: {
+            set: []
+          },
+          tasks: {
+            deleteMany: {}
+          },
+          statuses: {
+            deleteMany: {}
+          },
+        }
+      }),
+      prisma.project.delete({
+        where: {
+          id: req.query.id
+        }
+      })
+    ])
+
+    return res.status(200).send({})
   }
 
   return res.status(404).send({ error: 'unknown request method' })
