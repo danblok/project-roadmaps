7230ca9f392755442ac9878a81a43527037613f0
vacatixn <danytokmakov@gmail.com>
2023-05-04 13:19:47 +0500

    feat: add Loader

---
+++ apps/web/components/Loader.tsx
@@ -0,0 +1,18 @@
+import { ArrowPathIcon } from '@heroicons/react/20/solid'
+import React from 'react'
+
+function Loader() {
+  return (
+    <div className="absolute bg-slate-200 left-0 w-full top-0 h-full bg-opacity-50 z-10">
+      <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
+        <ArrowPathIcon
+          className="animate-spin text-cornflower-blue"
+          height={100}
+          width={100}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Loader
