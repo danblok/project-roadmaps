5ba467912f2c8b92008d5d0ae9189bb3db7cd751
vacatixn <danytokmakov@gmail.com>
2023-05-04 13:10:10 +0500

    feat: add profiles api handler

---
+++ apps/web/pages/api/profiles/index.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from 'database'
+import { getToken } from "next-auth/jwt";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const token = await getToken({ req })
+
+  if (req.method === 'GET') {
+    if (!req.query.filter || typeof req.query.filter !== 'string') {
+      return res.status(404).json({ error: 'query error' })
+    }
+    const accounts = await prisma.account.findMany({
+      where: {
+        OR: [
+          {
+            name: {
+              mode: "insensitive",
+              contains: req.query.filter
+            }
+          },
+          {
+            email: {
+              mode: "insensitive",
+              contains: req.query.filter
+            }
+          }
+        ]
+      },
+    })
+
+
+    return res.status(200).send({ data: accounts })
+  }
+
+  return res.status(404).send({ error: 'unknown request method' })
+}
