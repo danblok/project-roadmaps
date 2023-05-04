20aae2b90eb7759f83d07a4e40be59c58f8c18b9
vacatixn <danytokmakov@gmail.com>
2023-05-04 13:18:47 +0500

    feat: add Title component for pages

---
+++ apps/web/components/Title.tsx
@@ -0,0 +1,7 @@
+export const Title = ({ title }: { title: string }) => (
+  <div className="px-10 py-8 border-b border-gray-200">
+    <h2 className="text-5xl font-bold text-cornflower-blue">
+      {title}
+    </h2>
+  </div>
+)
