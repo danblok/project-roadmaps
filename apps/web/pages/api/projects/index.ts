897284793d4406a09504617bdb685ce80b1dfb3f
vacatixn <danytokmakov@gmail.com>
2023-05-04 13:14:52 +0500

    feat: update projects api handler

--- apps/web/pages/api/projects/index.ts
+++ apps/web/pages/api/projects/index.ts
@@ -9,5 +9,35 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(201).send({ data: project })
   }
 
+  if (req.method === 'GET') {
+    if (!req.query.accountId || typeof req.query.accountId !== 'string') {
+      return res.status(404).json({ error: 'query error' })
+    }
+    const projects = await prisma.project.findMany({
+      where: {
+        OR: [
+          {
+            ownerId: req.query.accountId,
+          },
+          {
+            contributors: {
+              some: {
+                id: req.query.accountId
+              },
+            },
+          },
+        ],
+      },
+      include: {
+        contributors: true
+      },
+      orderBy: {
+        name: 'asc',
+      },
+    })
+
+    return res.status(200).send({ data: projects })
+  }
+
   return res.status(404).send({ error: 'unknown request method' })
 }
