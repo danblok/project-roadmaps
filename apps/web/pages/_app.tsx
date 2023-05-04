d79866b795bb7e5ceda20728a293321e55f0d8df
vacatixn <danytokmakov@gmail.com>
2023-05-04 13:17:15 +0500

    feat: add QueryClient with hydration

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -4,23 +4,31 @@ import { SessionProvider } from 'next-auth/react'
 import { type AppProps } from 'next/app'
 import Layout from './layout'
 import {
+  DehydratedState,
+  Hydrate,
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
+import { useState } from 'react'
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps<{ session: Session }>) {
+}: AppProps<{
+  session: Session
+  dehydratedState: DehydratedState
+}>) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
     <QueryClientProvider client={queryClient}>
-      <SessionProvider session={session}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionProvider>
+      <Hydrate state={pageProps.dehydratedState}>
+        <SessionProvider session={session}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
+      </Hydrate>
     </QueryClientProvider>
   )
 }
