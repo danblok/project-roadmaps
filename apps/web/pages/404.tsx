3448fea66186e103ceaae01908e8ee1d5026de64
vacatixn <danytokmakov@gmail.com>
2023-05-04 13:15:19 +0500

    feat: add custom 404 page

---
+++ apps/web/pages/404.tsx
@@ -0,0 +1,16 @@
+export default function Page404() {
+  return (
+    <div className="h-full text-center flex flex-col items-center justify-center align-middle">
+      <div>
+        <h1 className="next-error-h1 inline-block mx-5 pr-6 text-2xl align-top leading-[49px]">
+          404
+        </h1>
+        <div className="inline-block text-left border-l border-l-gray-500 pl-8">
+          <h2 className="text-sm font-normal leading-[49px]">
+            This page could not be found.
+          </h2>
+        </div>
+      </div>
+    </div>
+  )
+}
