4f8f1ba266db073a1f50041865ddd981f160c567
vacatixn <danytokmakov@gmail.com>
2023-05-04 13:08:59 +0500

    feat: add profile queries

--- apps/web/api/profile.ts
+++ apps/web/api/profile.ts
@@ -1,7 +1,27 @@
 import axios from "axios"
-import { User } from "database"
+import { Prisma, Account } from "database"
+import { UpdateAccountInput } from "./types"
 
-export const getProfile = async (id: string): Promise<User | null> => {
+export const getProfile = async (id: string): Promise<(Account & {
+  ownedProjects: {
+    _count: Prisma.ProjectCountOutputType;
+  }[];
+  projects: {
+    _count: Prisma.ProjectCountOutputType;
+  }[];
+}) | null> => {
   return axios.get(`/api/profile/${id}`)
-    .then(({ data }: { data: User }) => data)
+    .then(({ data: { data } }) => data)
+}
+
+export const updateProfile = async (id: string, account: UpdateAccountInput) => {
+  return axios.post(`/api/profile/${id}`, account)
+}
+
+export const deleteProfile = async (id: string) => {
+  return axios.delete(`/api/profile/${id}`)
+}
+
+export const getProfiles = async (filter: string): Promise<Account[]> => {
+  return axios.get(`/api/profiles?filter=${filter}`).then(({ data: { data } }) => data)
 }
