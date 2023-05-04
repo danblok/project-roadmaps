30b825faba00a0a896bbe204a84d45d6adfa899f
vacatixn <danytokmakov@gmail.com>
2023-05-04 13:06:28 +0500

    feat: add types for api

--- apps/web/api/types.ts
+++ apps/web/api/types.ts
@@ -1,3 +1,6 @@
-import { Project } from "database";
+import { Project, Account } from "database";
 
 export type AddProjectInput = Pick<Project, 'name' | 'description' | 'ownerId'>
+export type UpdateAccountInput = Pick<Account, 'name'>
+export type UpdateProjectInput = Pick<Project, 'id'> & Partial<Pick<Project, 'description' | 'name'>>
+export type AddRemoveContributorInput = { projectId: string, contributorId: string }
