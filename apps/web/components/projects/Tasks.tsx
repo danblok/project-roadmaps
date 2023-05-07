3d856eada1e3d4f6cfc016e4105f8dc2a42a431d
vacatixn <danytokmakov@gmail.com>
2023-05-07 16:09:41 +0500

    feat: add a list of tasks

---
+++ apps/web/components/projects/Tasks.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import useProjectContext from './ProjectContext'
+import { TaskItem } from './TaskItem'
+import { Title } from '../Title'
+import { AddTaskItem } from './AddTaskItem'
+
+export const Tasks = () => {
+  const { project } = useProjectContext()
+  const [editableId, setEditableId] = useState('')
+
+  return (
+    <div>
+      <Title title="Tasks" />
+      <div className="flex flex-col p-4 gap-4">
+        <h3 className="text-xl font-bold text-cornflower-blue self-center lg:self-start">
+          Create a new task
+        </h3>
+        <AddTaskItem />
+        <h3 className="text-xl font-bold text-cornflower-blue self-center lg:self-start">
+          Manage your tasks
+        </h3>
+        {project.tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            editableId={editableId}
+            setEditableId={setEditableId}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
