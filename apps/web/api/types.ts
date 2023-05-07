46ffc857ba60f11bc8adb48232fb4793d5eff3f2
vacatixn <danytokmakov@gmail.com>
2023-05-07 16:07:11 +0500

    feat: add types for tasks and statuses inputs

--- apps/web/api/types.ts
+++ apps/web/api/types.ts
@@ -1,4 +1,4 @@
-import { Project, Account } from 'database'
+import { Project, Account, Task, Status } from 'database'
 
 export type AddProjectInput = Pick<Project, 'name' | 'description' | 'ownerId'>
 export type UpdateAccountInput = Pick<Account, 'name'>
@@ -8,3 +8,8 @@ export type AddRemoveContributorInput = {
   projectId: string
   contributorId: string
 }
+export type AddTaskInput = Pick<Task, 'name' | 'statusId' | 'until' | 'from'> &
+  Partial<Pick<Task, 'comments'>>
+export type UpdateTaskInput = Pick<Task, 'id'> &
+  Partial<Pick<Task, 'name' | 'comments' | 'statusId' | 'from' | 'until'>>
+export type AddStatusInput = Pick<Status, 'name'>
