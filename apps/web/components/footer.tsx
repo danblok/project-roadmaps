e3a003f416080a0c0cd9e47692790c1e0fd5b8ad
vacatixn <danytokmakov@gmail.com>
2023-05-08 23:03:33 +0500

    feat: add footer

--- apps/web/components/footer.tsx
+++ apps/web/components/footer.tsx
@@ -1,3 +1,12 @@
 export default function Footer() {
-  return <div className="row-start-3 row-end-4">Footer page</div>
+  return (
+    <div className="row-start-3 row-end-4">
+      <p className="mt-5 text-center text-sm leading-6 text-slate-500">
+        Made by Tokmakov Daniil
+      </p>
+      <p className="mt-5 text-center text-sm leading-6 text-slate-500">
+        © 2023 Quick Roadmaps. All rights reserved.
+      </p>
+    </div>
+  )
 }
