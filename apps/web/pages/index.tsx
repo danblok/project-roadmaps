2147bb2d2ca8588027edc025091a802f5581ee0c
vacatixn <danytokmakov@gmail.com>
2023-05-08 23:02:47 +0500

    feat: add hero page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,5 +1,5 @@
-import clsx from 'clsx'
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -7,13 +7,55 @@ export default function Home() {
       <Head>
         <title>Quick Roadmaps</title>
       </Head>
-      <div
-        className={clsx(
-          'text-2xl font-bold text-red-500',
-          'flex justify-center items-center'
-        )}
-      >
-        Home
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#F25F5C] to-[#5C95FF] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-2xl py-24 sm:py-40 lg:py-48">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Manage your projects{' '}
+              <span className="text-bittersweet">blazingly fast</span>
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              <span className="text-cornflower-blue font-bold">C</span>reate
+              your own tasks.{' '}
+              <span className="text-cornflower-blue font-bold">C</span>ontribute
+              with your teammates.{' '}
+              <span className="text-cornflower-blue font-bold">C</span>ontrol
+              your projects.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                href="/projects"
+                className="rounded-md bg-bittersweet px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cornflower-blue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
       </div>
     </>
   )
