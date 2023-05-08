8f727191ff7d864273d2cf1f2cc6c9f01a44ab7e
vacatixn <danytokmakov@gmail.com>
2023-05-08 23:01:29 +0500

    feat: add tasks and headers grouping

---
+++ apps/web/utils/groupTasks.ts
@@ -0,0 +1,60 @@
+import { Status, Task } from 'database'
+
+export default function groupTasks(tasks: (Task & { status: Status })[]) {
+  const groupedTasks = new Map<
+    string,
+    Map<number, Map<number, (Task & { status: Status })[]>>
+  >()
+
+  for (const task of tasks) {
+    const date = new Date(task.until)
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const status = task.status.name
+
+    if (groupedTasks.has(status)) {
+      const groupedTasksByStatus = groupedTasks.get(status)
+      // @ts-expect-error
+      if (groupedTasksByStatus.has(year)) {
+        // @ts-expect-error
+        const groupedTasksByYear = groupedTasksByStatus.get(year)
+        // @ts-expect-error
+        if (groupedTasksByYear.has(month)) {
+          // @ts-expect-error
+          groupedTasksByYear.get(month).push(task)
+        } else {
+          // @ts-expect-error
+          groupedTasksByYear.set(month, [task])
+        }
+      } else {
+        // @ts-expect-error
+        groupedTasksByStatus.set(year, new Map().set(month, [task]))
+      }
+    } else {
+      groupedTasks.set(
+        status,
+        new Map().set(year, new Map().set(month, [task]))
+      )
+    }
+  }
+
+  return groupedTasks
+}
+
+export function getUniqueYearAndMonthTaskHeaders(
+  tasks: (Task & { status: Status })[]
+) {
+  let headers: [number, number][] = new Array()
+
+  for (const task of tasks) {
+    const date = new Date(task.until)
+    const year = date.getFullYear()
+    const month = date.getMonth()
+
+    if (!headers.find((h) => h[0] === year && h[1] === month)) {
+      headers.push([year, month])
+    }
+  }
+
+  return headers
+}
