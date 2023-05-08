c9892b0fdc458170aaf507b5073672a59dfe5b62
vacatixn <danytokmakov@gmail.com>
2023-05-08 23:01:58 +0500

    chore: remove unused width

--- apps/web/pages/layout.tsx
+++ apps/web/pages/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: PropsWithChildren) {
     <div className="h-screen grid grid-rows-main-layout">
       <Header />
       <main className="bg-cornflower-blue bg-opacity-20 flex flex-col">
-        <div className="max-w-7xl w-7xl w-full mx-auto my-12 lg:my-18 xl:my-24 max-h-full flex-grow">
+        <div className="max-w-7xl w-full mx-auto my-12 lg:my-18 xl:my-24 max-h-full flex-grow">
           {children}
         </div>
       </main>
