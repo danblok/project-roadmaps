a6b97efd1dc949b41804413f5fe0e8cfd7816976
vacatixn <danytokmakov@gmail.com>
2023-05-20 16:29:39 +0500

    fix: the Home page doesn't overlap the navigation panel

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       </Head>
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          className="absolute inset-x-0 -top-40 -z-20 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
           <div
