de4c419b4556762285bb04cfcd9e8e019f17beac
vacatixn <danytokmakov@gmail.com>
2023-05-20 16:24:00 +0500

    fix: add space to the bottom of the footer

--- apps/web/components/footer.tsx
+++ apps/web/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <p className="mt-5 text-center text-sm leading-6 text-slate-500">
         Made by Tokmakov Daniil
       </p>
-      <p className="mt-5 text-center text-sm leading-6 text-slate-500">
+      <p className="my-5 text-center text-sm leading-6 text-slate-500">
         © 2023 Quick Roadmaps. All rights reserved.
       </p>
     </div>
