6d5c75755a65fef7bdd29897a8fad3897c29cd3d
vacatixn <danytokmakov@gmail.com>
2023-05-20 16:22:41 +0500

    fix: remove vertical line on small devices

--- apps/web/pages/404.tsx
+++ apps/web/pages/404.tsx
@@ -5,7 +5,7 @@ export default function Page404() {
         <h1 className="next-error-h1 inline-block mx-5 pr-6 text-2xl align-top leading-[49px]">
           404
         </h1>
-        <div className="inline-block text-left border-l border-l-gray-500 pl-8">
+        <div className="inline-block text-left sm:border-l sm:border-l-gray-500 pl-8">
           <h2 className="text-sm font-normal leading-[49px]">
             This page could not be found.
           </h2>
