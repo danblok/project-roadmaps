67a0ea4aeb95c919835e936e5658ab51715f77bc
vacatixn <danytokmakov@gmail.com>
2023-06-18 19:05:32 +0500

    fix: remove the user from the contributors searching

--- apps/web/pages/api/profiles/index.ts
+++ apps/web/pages/api/profiles/index.ts
@@ -3,36 +3,44 @@ import { prisma } from 'database'
 import { getToken } from 'next-auth/jwt'
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  const token = await getToken({ req })
+    const token = await getToken({ req })
 
-  if (req.method === 'GET') {
-    if (!req.query.filter || typeof req.query.filter !== 'string') {
-      return res.status(404).json({ error: 'query error' })
+    if (req.method === 'GET') {
+        if (!req.query.filter || typeof req.query.filter !== 'string') {
+            return res.status(404).json({ error: 'query error' })
+        }
+        const accounts = await prisma.account.findMany({
+            where: {
+                AND: [
+                    {
+                        id: { not: token?.accountId },
+                    },
+                    {
+                        OR: [
+                            {
+                                name: {
+                                    mode: 'insensitive',
+                                    contains: req.query.filter,
+                                },
+                            },
+                            {
+                                email: {
+                                    mode: 'insensitive',
+                                    contains: req.query.filter,
+                                },
+                            },
+                        ]
+                    },
+                ],
+            },
+        })
+        // .then(accounts => accounts.filter(account => account.id !== token?.accountId))
+
+        return res.status(200).send({ data: accounts })
     }
-    const accounts = await prisma.account.findMany({
-      where: {
-        OR: [
-          {
-            name: {
-              mode: 'insensitive',
-              contains: req.query.filter,
-            },
-          },
-          {
-            email: {
-              mode: 'insensitive',
-              contains: req.query.filter,
-            },
-          },
-        ],
-      },
-    })
 
-    return res.status(200).send({ data: accounts })
-  }
-
-  return res.status(404).send({ error: 'unknown request method' })
+    return res.status(404).send({ error: 'unknown request method' })
 }
